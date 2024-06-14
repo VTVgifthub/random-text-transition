d3db15687042da6bb3c4405e4b8d3fdb60791560
VTVcancode <86501740+VTVgifthub@users.noreply.github.com>
2024-06-13 23:30:01 -0300

    Add files via upload

---
+++ Random Text Transition.py
@@ -0,0 +1,13 @@
+import string
+import time
+text = 'Hello World'
+temp = ''
+for ch in text:
+    for i in string.printable:
+        if i == ch or ch == time.sleep(0.02):
+            print(temp+i)
+            temp +=ch
+            break
+        else:
+            time.sleep(0.02)
+            print(temp+i)
